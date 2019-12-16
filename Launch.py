4e1f47abfcb11e379becfb26c0e3bebd7d756f59
Abhinav Java <java.abhinav99@gmail.com>
2019-12-16 12:29:29 +0530

    Decentralized MOPSO code

---
+++ Launch.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import dronekit
+import getpass
+from time import sleep
+
+N = int(sys.argv[1])
+launch_string = "gnome-terminal"
+launch_string_mopso
+
+for i in range(N):
+	os.chdir("/home/"+getpass.getuser()+"/Swarm_Testing/")
+	launch_string += " --tab -e 'python3 modified_server.py " + str(i) +"'"
+	sleep(0.1)
+os.system(launch_string)
+
+sleep(3)
+
+for i in range(N):
+	os.chdir("/home/"+getpass.getuser()+"/Swarm_Testing/")
+	launch_string_mopso += " --tab -e 'python3 mopso.py " + str(i) +"'"
+	sleep(0.1)
+os.system(launch_string_mopso)
